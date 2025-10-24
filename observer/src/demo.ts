559139e5524c81d81a02b535e9353264600923cb
RodrigoAmaral <rodrigoamaral1705@gmail.com>
2025-10-23 23:22:58 -0300

    fix: retirada dos timeouts

--- observer/src/demo.ts
+++ observer/src/demo.ts
@@ -1,7 +1,6 @@
 import { alunoObserver } from "./observers/alunoObserver";
 import { entregadorObserver } from "./observers/entregadorObserver";
 import { fornecedorObserver } from "./observers/fornecedorObserver";
-import { Observer } from "./observers/observer";
 import { Subject } from "./subject";
 
 const subject = new Subject();
@@ -13,17 +12,13 @@ const fornecedor = new fornecedorObserver(subject);
 console.log("\n=== PodePedirFCTE: STATUS DO PEDIDO SUCULENTO  ===\n");
 
 subject.setState(1);
-setTimeout(() => subject.setState(2), 1000);
-setTimeout(() => subject.setState(3), 2000);
-setTimeout(() => subject.setState(4), 3000);
-setTimeout(() => subject.setState(5), 4000);
+subject.setState(2);
+subject.setState(3);
+subject.setState(4);
+subject.setState(5);
 
-setTimeout(() => {
-    console.log("\n--- O aluno saiu do sistema (detach) ---\n");
-    subject.detach(aluno);
-}, 2500);
+console.log("\n--- O aluno saiu do sistema (detach) ---\n");
+subject.detach(aluno);
 
-setTimeout(() => {
-    console.log("\n--- Mudança de estado após aluno sair ---\n");
-    subject.setState(4);
-}, 3500);
+console.log("\n--- Mudança de estado após aluno sair ---\n");
+subject.setState(4);
